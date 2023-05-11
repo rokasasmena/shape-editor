41d5cc99e0d6f747c2c05eba533200e98bdae552
Rokas <rokasasmena@gmail.com>
2023-05-11 19:52:24 +0300

    Project started. Initializing node and main files

---
+++ shape-editor/src/app/shape-edit/shape-editor-component/shape-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shape-edit',
+  templateUrl: './shape-edit.component.html',
+  styleUrls: ['./shape-edit.component.scss']
+})
+export class ShapeEditComponent {
+
+}
